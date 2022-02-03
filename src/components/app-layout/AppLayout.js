c671594e61779452e18435d76886a873f84f0c41
Laura <ltr@noaignite.com>
2022-02-03 20:30:16 +0100

    Basic layout

---
+++ src/components/app-layout/AppLayout.js
@@ -0,0 +1,15 @@
+import Footer from '../footer/footer';
+import Header from '../header/header';
+
+function AppLayout() {
+    return (
+      <div id="app-wrapper">
+        <Header />
+
+        <Footer />
+      </div>
+    );
+  }
+  
+  export default AppLayout;
+  
